a0fcf143d1356234aa8d3a96b2eed57379b458d8
Daniel Schopf <danielschopf04@gmail.com>
2025-04-07 22:45:49 -0300

    style: added sidebar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,31 @@
 import { lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "./components/SideBar";
 
-// const Home = lazy(() => import("./pages/Home")); 
-// TODO: manter a importação de pages com lazy para que no build ele divida o código em partes menores e carregue apenas o necessário, diminuindo tempo de renderização!
+// const Home = lazy(() => import("./pages/Home"));
+// TODO: manter a importação de pages com lazy para que no build ele divida o código em partes menores
 const PageNotFound = lazy(() => import("./views/Erro404"));
 const Listagem = lazy(() => import("./views/Listagem"));
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="" element={<h1>Home</h1> } />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="/listagem" element={<Listagem />} />
-      </Routes>
+      <Sidebar />
+      <div
+        style={{
+          marginLeft: "80px", // largura da sidebar colapsada
+          padding: "20px",
+          overflowY: "auto",
+          height: "100vh",
+        }}
+      >
+        <Routes>
+          <Route path="" element={<h1>Home</h1>} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/listagem" element={<Listagem />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ frontend/src/components/SideBar.tsx
@@ -0,0 +1,71 @@
+// components/Sidebar.tsx
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { HouseDoorFill, GearFill } from "react-bootstrap-icons";
+import { FaLeaf } from "react-icons/fa";
+import sideBarLogo from "../../public/fotos/Jerivaldo.png";
+import "../App.css";
+
+export default function MySidebar() {
+  const [collapsed, setCollapsed] = useState(true);
+
+  return (
+    <div
+      onMouseEnter={() => setCollapsed(false)}
+      onMouseLeave={() => setCollapsed(true)}
+      style={{ height: "100vh", position: "fixed", zIndex: 1000 }}
+    >
+      <Sidebar
+        collapsed={collapsed}
+        transitionDuration={300}
+        style={{
+          borderRight: "1px solid #01C061",
+          backgroundColor: "#fff",
+          height: "100vh",
+          color: "#005B60",
+        }}
+      >
+        <div
+          className="p-3 fw-bold fs-5 text-center d-flex align-items-center justify-content-center gap-2"
+          style={{ borderBottom: collapsed ? "" : "1px solid #01C061" }}
+        >
+          <img
+            src={sideBarLogo}
+            alt="Logo"
+            style={{
+              width: collapsed ? "50px" : "40px",
+              height: collapsed ? "50px" : "40px",
+              borderRadius: "50%",
+            }}
+          />
+          {!collapsed && <span>Jardim Botânico</span>}
+        </div>
+
+        <Menu>
+          <MenuItem
+            icon={<HouseDoorFill />}
+            component={<Link to="/" />}
+            className="sidebar-item"
+          >
+            Início
+          </MenuItem>
+          <MenuItem
+            icon={<FaLeaf />}
+            component={<Link to="/listagem" />}
+            className="sidebar-item"
+          >
+            Biodiversidade
+          </MenuItem>
+          <MenuItem
+            icon={<GearFill />}
+            component={<Link to="/configuracoes" />}
+            className="sidebar-item"
+          >
+            Configurações
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+    </div>
+  );
+}
