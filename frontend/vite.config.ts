bb8b10f9f145bad05bc47d1a3a061c88095648b0
Diogo <dapereira@inf.ufsm.br>
2025-07-09 13:26:44 -0300

    feat: new vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
+  build: {
     assetsDir: "static/react",
   },
   server: {
-    host: '0.0.0.0', 
+    host: "0.0.0.0",
     port: 3000,
+    allowedHosts: ["sitejardimbotanicoufsm-frontend.onrender.com"],
   },
-})
+});
